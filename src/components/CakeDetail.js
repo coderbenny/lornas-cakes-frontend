a1a9a12e5bf232ee477fc8000f3c2f72c2233c75
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-24 12:42:19 +0300

    Added the shopping cart to the order details component

--- src/components/CakeDetail.js
+++ src/components/CakeDetail.js
@@ -11,18 +11,69 @@ function CakeDetail() {
     }
 
     return (
-        <div className="items-center align-center bg-orange-500 h-[700px]">
+        <div className="items-center bg-orange-500 h-[700px] justify-center">
 
             <h1 className="font-bold text-3xl text-center text-white">Order Details</h1>
 
-            <div key={cake.id} className="flex flex-col rounded-md items-center bg-white p-3 w-[300px]">
-                <div className="mb-3">
-                    <img src={cake.image_url} alt={cake.image_alt} className="h-40 w-full" />
+            <div className="flex justify-center">
+                <div key={cake.id} className="flex flex-col mr-10 rounded-md mt-5 items-center bg-white p-3 w-[400px]">
+                    <div className="mb-3">
+                        <img src={cake.image_url} alt={cake.image_alt} className="h-full w-full" />
+                    </div>
+                    <h1 className="font-bold text-3xl">{cake.type}</h1>
+                    <p className="mb-2 text-lg">{cake.price}</p>
+                    <p className="mb-2 bg-gray-500 rounded-md text-white p-2">{cake.description}</p>
+                    <NavLink className="bg-black text-white w-full text-center p-1 rounded-md font-bold" to={`/buy/${id}`}>Buy</NavLink>
+                    <NavLink className="text-center bg-red-500 text-white w-full text-center p-1 font-bold rounded-md mt-2" to="/cakes">Cancel</NavLink>
                 </div>
-                <h1 className="font-bold ">{cake.type}</h1>
-                <p className="mb-2">{cake.price}</p>
-                <NavLink className="bg-black text-white w-full text-center p-1 rounded-md" to={`/buy/${id}`}>Buy</NavLink>
-                <NavLink className="text-center bg-red-500 text-white w-full text-center p-1 rounded-md mt-2" to="/cakes">Cancel</NavLink>
+
+                <div className="bg-white mr-10 mt-5 rounded-md w-[400px]">
+                    <h1 className="text-center mt-2 mb-2 font-bold text-2xl">Shopping Cart</h1>
+                    <div className="mx-auto w-[380px] p-3 h-[350px] overflow-y-auto rounded-md">
+
+                        {/* Cart Item */}
+                        <div className="flex p-2 bg-gray-300 rounded-md mb-3 shadow-lg items-center">
+                            <img src={cake.image_url} alt={cake.image_alt} className="rounded-md mr-2 h-[70px] w-[70px]" />
+                            <div className="max-h-full">
+                                <h1 className="font-bold text-lg">{cake.type}</h1>
+                                <p className=""><strong>Price:</strong> {cake.price}</p>
+                                <p className=""><strong>Quantity: x1</strong>{ }</p>
+                            </div>
+                        </div>
+
+                        <div className="flex p-2 bg-gray-300 rounded-md mb-3 shadow-lg items-center">
+                            <img src={cake.image_url} alt={cake.image_alt} className="rounded-md mr-2 h-[70px] w-[70px]" />
+                            <div className="max-h-full">
+                                <h1 className="font-bold text-lg">{cake.type}</h1>
+                                <p className=""><strong>Price:</strong> {cake.price}</p>
+                                <p className=""><strong>Quantity: x1</strong>{ }</p>
+                            </div>
+                        </div>
+
+                        <div className="flex p-2 bg-gray-300 rounded-md mb-3 shadow-lg items-center">
+                            <img src={cake.image_url} alt={cake.image_alt} className="rounded-md mr-2 h-[70px] w-[70px]" />
+                            <div className="max-h-full">
+                                <h1 className="font-bold text-lg">{cake.type}</h1>
+                                <p className=""><strong>Price:</strong> {cake.price}</p>
+                                <p className=""><strong>Quantity: x1</strong>{ }</p>
+                            </div>
+                        </div>
+
+                        <div className="flex p-2 bg-gray-300 rounded-md mb-3 shadow-lg items-center">
+                            <img src={cake.image_url} alt={cake.image_alt} className="rounded-md mr-2 h-[70px] w-[70px]" />
+                            <div className="max-h-full">
+                                <h1 className="font-bold text-lg">{cake.type}</h1>
+                                <p className=""><strong>Price:</strong> {cake.price}</p>
+                                <p className=""><strong>Quantity: x1</strong>{ }</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="flex mt-1 ml-2 items-center mx-auto mr-2 bg-black p-2">
+                        <h1 className="font-bold text-red-400 text-3xl mr-3">TOTAL:</h1>
+                        <p className="font-bold text-white text-2xl">$100.00</p>
+                    </div>
+                </div>
             </div>
 
         </div >
